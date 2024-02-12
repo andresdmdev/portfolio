c41d011538dc656688416d6b99f65906c6126424
Andres Marquez <andresdmf55@gmail.com>
2024-02-11 19:15:54 -0500

    Add initial components

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './home/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
