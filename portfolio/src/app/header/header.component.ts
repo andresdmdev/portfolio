112a7b9a0d104fd46ebf716de1c440a0f092ef32
Andres Marquez <andresdmf55@gmail.com>
2024-02-11 20:39:24 -0500

    Add styles config with theme logic in header

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  theme = "light";
 
+  ngOnInit(): void {
+    const isDarkPreferedTheme = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (isDarkPreferedTheme) {
+      this.setTheme("dark");
+    }
+  }
+
+  setTheme(theme: string) {
+
+    if (theme === 'light')
+      theme = 'dark';
+    else
+      theme = 'light';
+
+    this.theme = theme;
+    document.documentElement.className = theme;
+  }
 }
