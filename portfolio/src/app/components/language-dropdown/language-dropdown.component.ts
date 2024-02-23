a1578e6ea20092c36c6d5644d04bada9909a5907
Andres Marquez <andresdmf55@gmail.com>
2024-02-22 23:12:48 -0500

    Add logic of language component

--- portfolio/src/app/components/language-dropdown/language-dropdown.component.ts
+++ portfolio/src/app/components/language-dropdown/language-dropdown.component.ts
@@ -1,19 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-language-dropdown',
   templateUrl: './language-dropdown.component.html',
   styleUrls: ['./language-dropdown.component.scss']
 })
-export class LanguageDropdownComponent {
-  readonly LANGUAGES = ['English', 'Español'];
-  readonly ICONLANGUAGES = ['us-flag.svg', 'us-flag.svg'];
+export class LanguageDropdownComponent implements OnInit {
+  readonly LANGUAGES = [
+    {
+      name: 'English',
+      url: 'us-flag.svg'
+    },
+    {
+      name: 'Español',
+      url: 'us-flag.svg'
+    },
+  ];
 
-  currentLanguage: string = this.LANGUAGES[0];
-  currentImage: string = this.ICONLANGUAGES[0]
+  currentLanguage: string = this.LANGUAGES[0].name;
+  currentImage: string = this.LANGUAGES[0].url;
   showOptions: boolean = false;
+
+  ngOnInit(): void {
+    if (localStorage.getItem('currentLanguage')) {
+      this.currentLanguage = JSON.parse(localStorage.getItem('currentLanguage')!) as string;
+    }
+  }
 
   toggleOptions() {
     this.showOptions = !this.showOptions;
+  }
+
+  selectLanguage() {
+
+    const language = this.nextlanguage()
+
+    if (this.currentLanguage !== language) {
+      this.currentLanguage = language;
+      localStorage.setItem('currentLanguage', JSON.stringify(language));
+    }
+  }
+
+  getUrlImage(): string {
+    const url = this.LANGUAGES[0].name == "English" ? this.LANGUAGES[1].url : this.LANGUAGES[0].url;
+    return `../../../assets/images/${url}`;
+  }
+
+  nextlanguage() {
+    return this.LANGUAGES.filter(x => x.name != this.currentLanguage)[0].name;
   }
 }
